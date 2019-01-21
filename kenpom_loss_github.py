4c5e30ba1739c7c93d083f80b23819ebcc725a08
jbenfleming <43318569+jbenfleming@users.noreply.github.com>
2019-01-21 13:20:44 -0600

    Update kenpom_loss_github.py

--- kenpom_loss_github.py
+++ kenpom_loss_github.py
@@ -8,11 +8,11 @@ r = requests.get(url).text
 soup = BeautifulSoup(r, 'lxml')
 
 #set file name and write headers
-path = 'C:\\Users\\username\\Desktop'
+path = 'C:\\Users\\usernamehere\\Desktop'
 filename = 'kp_scrape_wl.csv'
 complete_name = os.path.join(path, filename)
 f = open(complete_name,'w')
-headers = 'AdjEM, Best, # of losses, Worst, AdjEM \n'
+headers = 'Rank, AdjEM, Best, # of losses, Worst, Rank, AdjEM \n'
 f.write(headers)
 
 #get all team data rows
@@ -44,6 +44,7 @@ for x in range(min_losses,count+1):
         rows = team.findAll('td')
         AdjEM = rows[4].text
         AdjEM = float(AdjEM)
+        rank = rows[0].text
         record = (rows[3].text)
         w,l = record.split('-')
         l = int(l)
@@ -51,13 +52,15 @@ for x in range(min_losses,count+1):
             if AdjEM > max_AdjEM:
                 max_AdjEM = AdjEM
                 max_name = name
+                max_rank = rank
                 max_check = True
-                
+               
     #find min_AdjEM
     for team in teams:
         name = team.find('td',{'class':'next_left'}).a.text
         rows = team.findAll('td')
         AdjEM = rows[4].text
+        rank = rows[0].text
         AdjEM = float(AdjEM)
         record = (rows[3].text)
         w,l = record.split('-')
@@ -66,8 +69,9 @@ for x in range(min_losses,count+1):
             if AdjEM < min_AdjEM:
                 min_AdjEM = AdjEM
                 min_name = name
+                min_rank = rank
                 min_check = True
-                
+                           
     #check to find mising loss numbers
     if not max_check:
         max_AdjEM = ' '
@@ -77,7 +81,8 @@ for x in range(min_losses,count+1):
         max_name = ' '
       
     #write values to csv
-    f.write(str(max_AdjEM) + "," + max_name + "," + 
-            str(x) + "," + min_name + "," + str(min_AdjEM) + '\n')
+    f.write(str(max_rank) + "," + str(max_AdjEM) + "," + max_name + "," + 
+            str(x) + "," + min_name + "," + str(min_rank) + "," + 
+            str(min_AdjEM) + '\n')
           
 f.close()
